a3bbe11493fdd9c058d46b52f3553d3eb7d7d6ed
Rashmika Rathnayaka <rashmikarathnayaka01@gmail.com>
2024-10-27 18:31:16 +0530

    change

--- src/components/pageProps/shopPage/ShopSideNav.js
+++ src/components/pageProps/shopPage/ShopSideNav.js
@@ -1,25 +1,20 @@
 import React from "react";
 import Category from "./shopBy/Category";
-import Price from "./shopBy/Price";
+//import Price from "./shopBy/Price";
 
 const ShopSideNav = () => {
   return (
     <div className="w-full bg-white">
       <div className="w-full flex flex-col gap-6">
         <button
-          className="bg-gray-200 text-black font-bold py-2 px-4 rounded hover:bg-gray-300"
+          className="bg-purple-500 text-white font-bold py-3 px-15 rounded hover:bg-blue-500"
           onClick={() => console.log("Category clicked!")}
         >
           <Category icons={false} />
         </button>
 
         {/* Price button */}
-        <button
-          className="bg-gray-200 text-black font-bold py-2 px-4 rounded hover:bg-gray-300"
-          onClick={() => console.log("Price clicked!")}
-        >
-          <Price />
-        </button>
+        
       </div>
     </div>
   );
